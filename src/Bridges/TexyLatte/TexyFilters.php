250a9cc1ea9448709eead8999bc6d4487dcfff57
Petr Morávek [Xificurk] <petr@pada.cz>
2016-05-19 23:54:44 +0200

    typo

--- src/Bridges/TexyLatte/TexyFilters.php
+++ src/Bridges/TexyLatte/TexyFilters.php
@@ -37,7 +37,7 @@ class TexyFilters extends Nette\Object
      */
     public function process(FilterInfo $filterInfo, $text, $singleLine = false)
     {
-        if (!in_array($filterInfo->contentType, [NULL, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], TRUE)) {
+        if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
             trigger_error("Filter |texy used with incompatible type " . strtoupper($filterInfo->contentType), E_USER_WARNING);
         }
 
@@ -52,7 +52,7 @@ class TexyFilters extends Nette\Object
      */
     public function processLine(FilterInfo $filterInfo, $text)
     {
-        if (!in_array($filterInfo->contentType, [NULL, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], TRUE)) {
+        if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
             trigger_error("Filter |texyLine used with incompatible type " . strtoupper($filterInfo->contentType), E_USER_WARNING);
         }
 
@@ -67,7 +67,7 @@ class TexyFilters extends Nette\Object
      */
     public function processTypo(FilterInfo $filterInfo, $text)
     {
-        if (!in_array($filterInfo->contentType, [NULL, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], TRUE)) {
+        if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
             trigger_error("Filter |texyTypo used with incompatible type " . strtoupper($filterInfo->contentType), E_USER_WARNING);
         }
 
