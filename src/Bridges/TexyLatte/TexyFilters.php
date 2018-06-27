1bb4c5f88ccf9d842d7cfe30c7c6d2a2bcf5763e
Petr Morávek <petr@pada.cz>
2018-06-27 22:47:08 +0200

    Improve type safety by explicite type casts

--- src/Bridges/TexyLatte/TexyFilters.php
+++ src/Bridges/TexyLatte/TexyFilters.php
@@ -25,7 +25,7 @@ class TexyFilters
     public function process(FilterInfo $filterInfo, string $text, bool $singleLine = false): string
     {
         if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
-            trigger_error('Filter |texy used with incompatible type ' . strtoupper($filterInfo->contentType), E_USER_WARNING);
+            trigger_error('Filter |texy used with incompatible type ' . strtoupper((string) $filterInfo->contentType), E_USER_WARNING);
         }
 
         $filterInfo->contentType = ($this->texyMultiplier->getOutputMode() & Texy::XML) ? Engine::CONTENT_XHTML : Engine::CONTENT_HTML;
@@ -35,7 +35,7 @@ class TexyFilters
     public function processLine(FilterInfo $filterInfo, string $text): string
     {
         if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
-            trigger_error('Filter |texyLine used with incompatible type ' . strtoupper($filterInfo->contentType), E_USER_WARNING);
+            trigger_error('Filter |texyLine used with incompatible type ' . strtoupper((string) $filterInfo->contentType), E_USER_WARNING);
         }
 
         $filterInfo->contentType = ($this->texyMultiplier->getOutputMode() & Texy::XML) ? Engine::CONTENT_XHTML : Engine::CONTENT_HTML;
@@ -45,7 +45,7 @@ class TexyFilters
     public function processTypo(FilterInfo $filterInfo, string $text): string
     {
         if (!in_array($filterInfo->contentType, [null, Engine::CONTENT_TEXT, Engine::CONTENT_HTML, Engine::CONTENT_XHTML, Engine::CONTENT_XML], true)) {
-            trigger_error('Filter |texyTypo used with incompatible type ' . strtoupper($filterInfo->contentType), E_USER_WARNING);
+            trigger_error('Filter |texyTypo used with incompatible type ' . strtoupper((string) $filterInfo->contentType), E_USER_WARNING);
         }
 
         return $this->texyMultiplier->processTypo($text);
